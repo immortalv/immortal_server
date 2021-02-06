c7989c34d145468df7c1bfcb68e9849d1b6ca3da
andrii-amplify <atykhan@amplify.com>
2021-02-06 18:27:26 +0200

    Update config json

--- src/config/config.js
+++ src/config/config.js
@@ -15,11 +15,8 @@ const envVarsSchema = Joi.object()
     JWT_SECRET: Joi.string().required().description('JWT secret key'),
     JWT_ACCESS_EXPIRATION_MINUTES: Joi.number().default(30).description('minutes after which access tokens expire'),
     JWT_REFRESH_EXPIRATION_DAYS: Joi.number().default(30).description('days after which refresh tokens expire'),
-    SMTP_HOST: Joi.string().description('server that will send the emails'),
-    SMTP_PORT: Joi.number().description('port to connect to the email server'),
-    SMTP_USERNAME: Joi.string().description('username for email server'),
-    SMTP_PASSWORD: Joi.string().description('password for email server'),
-    EMAIL_FROM: Joi.string().description('the from field in the emails sent by the app'),
+    USER_EMAIL: Joi.string().description('username for email server'),
+    USER_PASSWORD: Joi.string().description('password for email server'),
   })
   .unknown();
 
