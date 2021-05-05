f0f5ae24823bd3a0bf5611791f1bb5ab08b0f631
andrii-amplify <atykhan@amplify.com>
2021-05-05 08:32:38 +0300

    Update profile route

--- src/services/profile.service.js
+++ src/services/profile.service.js
@@ -1,3 +1,5 @@
+const httpStatus = require('http-status');
+const ApiError = require('../utils/ApiError');
 const { Profile } = require('../models');
 
 /**
@@ -15,13 +17,50 @@ const getProfiles = async (userId) => {
   return profiles;
 };
 
-const getProfile = async (id) => {
-  const profile = await Profile.findById(id);
+/**
+ * Get profile by id
+ * @param {ObjectId} id
+ * @returns {Promise<User>}
+ */
+const getProfileById = async (id) => {
+  return Profile.findById(id);
+};
+
+/**
+ * Update profile by id
+ * @param {ObjectId} profileId
+ * @param {Object} updateBody
+ * @returns {Promise<Profile>}
+ */
+const updateProfileById = async (userId, updateBody) => {
+  const profile = await getProfileById(userId);
+  if (!profile) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
+  }
+
+  Object.assign(profile, updateBody);
+  await profile.save();
+  return profile;
+};
+
+/**
+ * Delete profile by id
+ * @param {ObjectId} profileId
+ * @returns {Promise<Profile>}
+ */
+const deleteProfileById = async (profileId) => {
+  const profile = await getProfileById(profileId);
+  if (!profile) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Profile not found');
+  }
+  await profile.remove();
   return profile;
 };
 
 module.exports = {
   createProfile,
   getProfiles,
-  getProfile,
+  getProfileById,
+  updateProfileById,
+  deleteProfileById,
 };
